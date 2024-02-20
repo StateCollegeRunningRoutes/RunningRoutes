eefd85aa067a511d2bc6b8a1332b99685940f205
Seth Roth <scr5471@psu.edu>
2024-02-20 15:48:33 -0500

    First change to project

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('state-college-video');
+    video.play();
+});
+
+
+function myFunction1() {
+    alert("You raised money for THON!");
+}
+
+function myFunction2() {
+    window.location.href = "https://earth.google.com/web/@40.79790273,-77.87642393,365.22366978a,16310.53725472d,35y,317.07736226h,0t,0r/data=MikKJwolCiExVWpudlk1cXd5M1VxdUJ5NjVyX2JBemFDM3Q1Z2pyenEgAToDCgEy";
+    
+}
+
+function myFunction3() {
+    window.location.href = "https://earth.google.com/web/@40.79237272,-77.85526002,345.7879951a,3016.17945226d,30.00001437y,359.99995888h,0t,0r/data=MikKJwolCiExVGdHVXVYZmhyeTFvMjktVkQ3SUY1c2tOWXQyX1piMGMgAToDCgEy";
+    
+}
+function myFunction4() {
+    window.location.href = "https://earth.google.com/web/@40.79977953,-77.90585827,395.47009001a,4115.15231993d,30y,0h,0t,0r/data=MikKJwolCiExb09MWGM5ZjRkYktWa19qbXQwZGhLYktCeEcxRmJOdHMgAToDCgEy";
+    
+}
+function myFunction5() {
+    alert("This page will be up and running soon");
+    
+}
+
+    
+
