6676e2801f45d006cca0e474639f98151dadfced
Seth Roth <scr5471@psu.edu>
2024-02-21 00:08:49 -0500

    Version2

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function myFunction1() {
-    alert("You raised money for THON!");
+    window.open("https://donate.thon.org/index.cfm?fuseaction=donorDrive.participant&participantID=92653", "_blank");
 }
 
 function myFunction2() {
-    window.location.href = "https://earth.google.com/web/@40.79790273,-77.87642393,365.22366978a,16310.53725472d,35y,317.07736226h,0t,0r/data=MikKJwolCiExVWpudlk1cXd5M1VxdUJ5NjVyX2JBemFDM3Q1Z2pyenEgAToDCgEy";
+    window.open("https://earth.google.com/web/@40.79790273,-77.87642393,365.22366978a,16310.53725472d,35y,317.07736226h,0t,0r/data=MikKJwolCiExVWpudlk1cXd5M1VxdUJ5NjVyX2JBemFDM3Q1Z2pyenEgAToDCgEy", "_blank");
     
 }
 
 function myFunction3() {
-    window.location.href = "https://earth.google.com/web/@40.79237272,-77.85526002,345.7879951a,3016.17945226d,30.00001437y,359.99995888h,0t,0r/data=MikKJwolCiExVGdHVXVYZmhyeTFvMjktVkQ3SUY1c2tOWXQyX1piMGMgAToDCgEy";
+    window.open("https://earth.google.com/web/@40.79237272,-77.85526002,345.7879951a,3016.17945226d,30.00001437y,359.99995888h,0t,0r/data=MikKJwolCiExVGdHVXVYZmhyeTFvMjktVkQ3SUY1c2tOWXQyX1piMGMgAToDCgEy", "_blank");
     
 }
 function myFunction4() {
-    window.location.href = "https://earth.google.com/web/@40.79977953,-77.90585827,395.47009001a,4115.15231993d,30y,0h,0t,0r/data=MikKJwolCiExb09MWGM5ZjRkYktWa19qbXQwZGhLYktCeEcxRmJOdHMgAToDCgEy";
+    window.open("https://earth.google.com/web/@40.79977953,-77.90585827,395.47009001a,4115.15231993d,30y,0h,0t,0r/data=MikKJwolCiExb09MWGM5ZjRkYktWa19qbXQwZGhLYktCeEcxRmJOdHMgAToDCgEy", "_blank");
     
 }
 function myFunction5() {
